27b7dea7ae553f3c31d5d9382483cecaa5ed6402
JulioRobocop <juliocesar.gp44@gmail.com>
2026-05-12 13:55:56 -0300

    feat: Initial development

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,29 @@
+<script setup lang="ts">
+import router from '@/router';
+import axios from 'axios';
+import { ref, onMounted } from 'vue';
+
+const listings = ref([])
+
+const fetchListings = async () => {
+  try {
+    const response = await axios.get("http://localhost:8081/")
+
+  } catch (error) {
+    
+  }
+}
+
+
+onMounted(() => {
+
+
+})
+
+
+
+</script>
+
+
 <template>
-  <div>
-    <h1>
-      Home
-    </h1>
-  </div>
 </template>
