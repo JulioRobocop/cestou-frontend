86c691c1d80f7f972c05eb974a975fa88abf595e
JulioRobocop <juliocesar.gp44@gmail.com>
2026-06-12 13:37:43 -0300

    feat: Add conclude confirmation

---
+++ src/components/ConcludedListingButton.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import api from '@/utils/api';
+import { ref } from 'vue';
+import { X } from 'lucide-vue-next';
+
+const success = ref(false)
+const error = ref('')
+const emit = defineEmits(['close', 'concluded'])
+
+const props = defineProps<{
+    listingId: number,
+}>()
+
+async function concludedListing(listingId: number) {
+    try {
+        await api.put(`/listings/${listingId}/conclude`);
+        success.value = true
+        emit('close')
+        emit('concluded')
+    } catch (err) {
+        error.value = "Não foi possível cancelar o anúncio.";
+        console.log("Error message: ", err);
+    }
+}
+
+</script>
+<template>
+    <div class="fixed inset-0 bg-black/50 flex items-center justify-center">
+        <div class="bg-white rounded-lg p-6 shadow-lg max-w-md w-full">
+            <button @click="emit('close')">
+                <X
+                    class="hover:rotate-180 cursor-pointer hover:bg-gray-300 transition-all duration-200  rounded-lg w-6 h-6" />
+            </button>
+            <h1 class="flex flex-col items-center mb-1">Concluir anúncio?</h1>
+            <div class=" bg-black w-full h-0.5 rounded-lg" />
+            <div v-if="success === true">
+                Anúncio concluído com sucesso
+            </div>
+            <div v-if="error">
+                Erro ao concluído o anúncio
+            </div>
+            <div class="gap-10 mt-7 flex flex-row justify-center items-center">
+                <button
+                    class="bg-green-400 p-4 cursor-pointer hover:p-6 hover:shadow-2xl hover:bg-green-300 transition-all rounded-2xl"
+                    @click="concludedListing(props.listingId)">Confirmar</button>
+                <button
+                    class="bg-red-400 p-4 cursor-pointer hover:p-6 hover:shadow-2xl hover:bg-red-300 transition-all rounded-2xl"
+                    @click="emit('close')">Cancelar</button>
+            </div>
+        </div>
+    </div>
+</template>
