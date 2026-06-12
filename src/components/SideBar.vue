12a2f1b421334a623dbf7a7f9f43c352bc6d504c
JulioRobocop <juliocesar.gp44@gmail.com>
2026-06-12 13:49:12 -0300

    feat: Add greetings for user

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,13 +1,22 @@
 <script setup lang="ts">
+import { Home, ClipboardList, LogOut, ChevronRightIcon, CircleUserRoundIcon } from 'lucide-vue-next'
+import { getUserFromToken } from '@/utils/getUserFromToken'
 import { ref } from 'vue'
-import { Home, ClipboardList, LogOut, ChevronRightIcon } from 'lucide-vue-next'
 
 const isExpanded = ref(localStorage.getItem("is_expanded") === "true")
+const user = getUserFromToken()
+const userName = ref(user || 'Usuário')
 
 const toggleMenu = () => {
   isExpanded.value = !isExpanded.value
   localStorage.setItem("is_expanded", String(isExpanded.value))
 }
+
+const logOut = () => {
+  const token = localStorage.getItem("token")
+  if (token) localStorage.removeItem("token")
+}
+
 </script>
 
 <template>
@@ -24,7 +33,14 @@ const toggleMenu = () => {
       </button>
     </div>
 
-    <span class="text-xs uppercase text-white mb-2 transition-opacity duration-300"
+    <div class="flex items-center gap-4">
+      <CircleUserRoundIcon class="w-6 h-6 min-w-6" />
+      <span class="whitespace-nowrap transition-opacity duration-300" :class="isExpanded ? 'opacity-100' : 'opacity-0'">
+        Olá, {{ userName }}
+      </span>
+    </div>
+
+    <span class="text-xs uppercase text-white mb-2 mt-2 transition-opacity duration-300"
       :class="isExpanded ? 'opacity-100' : 'opacity-0'">
       Menu
     </span>
@@ -51,7 +67,7 @@ const toggleMenu = () => {
     <div class="flex-1"></div>
 
     <nav class="-mx-4">
-      <router-link to="/login"
+      <router-link @click="logOut()" to="/login"
         class="flex items-center gap-4 px-4 py-3 hover:bg-gray-600 transition-colors duration-200">
         <LogOut class="w-6 h-6 min-w-6" />
         <span class="whitespace-nowrap transition-opacity duration-300"
