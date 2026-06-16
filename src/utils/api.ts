6148c1bde956cdd25e47a95b9f60459be34d7cd4
JulioRobocop <juliocesar.gp44@gmail.com>
2026-06-16 17:43:39 -0300

    feat: Add credentials to api axios

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,17 +3,10 @@ import router from "@/router";
 
 const api = axios.create({
   baseURL: "http://localhost:8081",
+  withCredentials: true,
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
-
-api.interceptors.request.use(
+api.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response?.status === 403) {
