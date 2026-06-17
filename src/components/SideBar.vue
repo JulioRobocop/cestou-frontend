5f4d029ccf189350991484d5ef6c5061e9c55330
JulioRobocop <juliocesar.gp44@gmail.com>
2026-06-17 10:15:01 -0300

    refactor: get the user first name from getUserFromUserName function

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
+import api from '@/utils/api'
 import { Home, ClipboardList, LogOut, ChevronRightIcon, CircleUserRoundIcon } from 'lucide-vue-next'
-import { getUserFromToken } from '@/utils/getUserFromToken'
+import { getUserFromUserName } from '@/utils/getUserFromUserName'
 import { ref } from 'vue'
 import router from '@/router'
 
 const isExpanded = ref(localStorage.getItem("is_expanded") === "true")
-const user = getUserFromToken()
+const user = getUserFromUserName()
 const userName = ref(user || 'Usuário')
 
 const toggleMenu = () => {
@@ -13,15 +14,20 @@ const toggleMenu = () => {
   localStorage.setItem("is_expanded", String(isExpanded.value))
 }
 
-function logout() {
-  localStorage.removeItem('token')
+async function logout() {
+  await api.post("/employees/logout")
+  localStorage.removeItem('userName')
   router.push("/login")
 }
+
 
 </script>
 
 <template>
-  <aside class="flex flex-col bg-[#ed0c6e] text-white min-h-screen p-4 transition-all duration-200 overflow-hidden"
+  <div v-if="isExpanded" class="fixed inset-0 bg-black/50 z-40 lg:hidden transition-opacity duration-300"
+    @click="toggleMenu" />
+  <aside
+    class="flex flex-col bg-[#ed0c6e] text-white min-h-screen p-4 overflow-hidden z-50 fixed lg:relative transition-all duration-300 ease-in-out"
     :class="isExpanded ? 'w-60' : 'w-16'">
     <div class="mb-4">
       <img src="/images/pado_logo.svg" alt="Logo" class="size-full rounded" />
@@ -47,7 +53,8 @@ function logout() {
     </span>
 
     <nav class="flex flex-col gap-1 -mx-4">
-      <router-link to="/" class="flex items-center gap-4 px-4 py-3 hover:bg-[#4F1528] transition-colors duration-200">
+      <router-link to="/" @click="toggleMenu"
+        class="flex items-center gap-4 px-4 py-3 hover:bg-[#4F1528] transition-colors duration-200">
         <Home class="w-6 h-6 min-w-6" />
         <span class="whitespace-nowrap transition-opacity duration-300"
           :class="isExpanded ? 'opacity-100' : 'opacity-0'">
@@ -55,7 +62,7 @@ function logout() {
         </span>
       </router-link>
 
-      <router-link to="/my-listings"
+      <router-link to="/my-listings" @click="toggleMenu"
         class="flex items-center gap-4 px-4 py-3 hover:bg-[#4F1528] transition-colors duration-200">
         <ClipboardList class="w-6 h-6 min-w-6" />
         <span class="whitespace-nowrap transition-opacity duration-300"
@@ -67,7 +74,7 @@ function logout() {
 
     <div class="flex-1"></div>
 
-    <nav class="-mx-4">
+    <nav class="flex flex-col gap-1 -mx-4">
       <button @click="logout" to="/login"
         class="flex items-center gap-4 px-4 py-3 hover:bg-[#4F1528] transition-colors duration-200">
         <LogOut class="w-6 h-6 min-w-6" />
