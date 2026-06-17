9ebcfd58b5f90bda56b7406e2a4f7f0d08351d5c
JulioRobocop <juliocesar.gp44@gmail.com>
2026-06-17 17:31:50 -0300

    feat: start development listing filters

---
+++ src/components/ListingFilter.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { sectorOptions } from '@/utils/sectorOptions';
+import { workShiftOptions } from '@/utils/workShiftOptions';
+import { ref } from 'vue';
+
+const selectSector = ref(sectorOptions)
+const selectWorkShift = ref(workShiftOptions)
+
+
+
+
+
+
+
+
+
+</script>
